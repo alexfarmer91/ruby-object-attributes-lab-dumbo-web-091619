ca3009a0d65b18ccbec17b301e7117852c51ed01
Alex Farmer <farmale10@gmail.com>
2019-09-18 02:48:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,3 +16,24 @@ class Dog
     @breed
   end 
 end
+
+class Person 
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+  
+  def job
+    @job
+  end 
+  
+  
+end 
